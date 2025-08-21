d1511f0f9e41156845f5d47a0b1a73b2a91fc6ae
Sagar <garodesrg24@gmail.com>
2025-08-21 20:04:30 +0530

    Husky issue resolved

---
+++ eslint.config.cjs
@@ -0,0 +1,57 @@
+const js = require("@eslint/js")
+const globals = require("globals")
+const tsParser = require("@typescript-eslint/parser")
+const tsPlugin = require("@typescript-eslint/eslint-plugin")
+const reactPlugin = require("eslint-plugin-react")
+
+module.exports = [
+  {
+    files : ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions : {
+      ecmaVersion : "latest",
+      sourceType: "module",
+    },
+    rules : {
+      ...js.configs.recommended.rules,
+      "no-unused-vars" : "warn",
+      "no-undef": "error",
+      "no-console": "warn",
+    }
+  },
+
+  {
+    files : ["backend/**/*.{js,ts}"],
+    languageOptions : {
+      globals: { 
+        ...globals.node
+      }
+    }
+  },
+
+  {
+    files : ["frontend/**/*.{js,ts,tsx,jsx}"],
+    languageOptions : {
+      parser : tsParser,
+      parserOptions : {
+        ecmaVersion : "latest",
+        sourceType: "module",
+        ecmaFeatures :  { jsx : true }
+      },
+      globals: { 
+        ...globals.browser
+      }
+    },
+    rules : {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      "react-hooks/exhaustive-deps" : "off",
+      "no-empty" : "off",
+      "react/react-in-jsx-scope" : "off",
+      "react/prop-types": "off"
+    },
+    plugins : {
+      "@typescript-eslint" : tsPlugin,
+      react : reactPlugin
+    }
+  },
+]
--- eslint.config.js
+++
@@ -1,5 +0,0 @@
-module.export = {
-  rules: {
-    "react-hooks/exhaustive-deps": "off",
-  },
-};
